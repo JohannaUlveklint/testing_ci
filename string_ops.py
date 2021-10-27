5aac69e632bfea49a3823e1d1d74e82206918b60
JohannaUlveklint <johanna.ulveklint@gmail.com>
2021-10-27 11:20:13 +0200

    Added robbers language translation function.

--- string_ops.py
+++ string_ops.py
@@ -6,8 +6,22 @@ def remove_vowels(word):
             new_word += character
     return new_word
 
+
 def reversed_string(word):
     return word[::-1]
+
+
+def translate_to_robber(text):
+    vowels = 'aeiouy'
+    robber_str = ''
+
+    for c in text:
+        if c not in vowels and c.isalpha():
+            robber_str += c + 'o' + c
+        else:
+            robber_str += c
+
+    return robber_str
 
 
 def main():
--- test_string_ops.py
+++ test_string_ops.py
@@ -7,3 +7,6 @@ class TestStringOps:
 
     def test_reversed_string(self):
         assert 'mikaoJ' == string_ops.reversed_string('Joakim')
+
+    def test_translate_to_robber(self):
+        assert string_ops.translate_to_robber('this is fun') == 'tothohisos isos fofunon'
